15ce331192ef412e33a2e43acc4547ace187066e
Rolando Santamaria Maso <kyberneees@gmail.com>
2019-06-28 15:05:55 +0200

    adding hooks demo

---
+++ demos/hooks.js
@@ -0,0 +1,27 @@
+const gateway = require('./../index')
+const PORT = process.env.PORT || 8080
+
+gateway({
+  routes: [{
+    prefix: '/service',
+    target: 'http://127.0.0.1:3000',
+    hooks: {
+      async onRequest (req, res) {
+        // you can alter the request object here
+        // adding headers:
+        req.headers['x-header'] = 'value'
+      },
+      rewriteHeaders (headers) {
+        // you can alter response headers here
+        return headers
+      },
+      onResponse (req, res, stream) {
+        // you can alter the origin response and remote response here
+        // default implementation explained here:
+        // https://www.npmjs.com/package/fast-gateway#onresponse-hook-default-implementation
+      }
+    }
+  }]
+}).start(PORT).then(server => {
+  console.log(`API Gateway listening on ${PORT} port!`)
+})
