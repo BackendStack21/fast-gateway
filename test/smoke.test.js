c14629100639de7020712b5f85aeba270f2a593d
Rolando Santamaria Maso <kyberneees@gmail.com>
2019-08-27 20:55:57 +0200

    linting

--- test/smoke.test.js
+++ test/smoke.test.js
@@ -1,4 +1,4 @@
-/* global describe, it, setImmediate */
+/* global describe, it */
 const expect = require('chai').expect
 const request = require('supertest')
 const fastGateway = require('./../index')
