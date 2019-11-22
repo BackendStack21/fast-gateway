cd8f2239fbc099146ee13a49a75f143ff724a95c
Rolando Santamaria Maso <kyberneees@gmail.com>
2019-11-22 14:03:28 +0100

    adding rates limiter example

---
+++ demos/rate-limit.js
@@ -0,0 +1,30 @@
+const gateway = require('../index')
+const rateLimit = require('express-rate-limit')
+const requestIp = require('request-ip')
+
+gateway({
+  middlewares: [
+    // acquire request IP
+    (req, res, next) => {
+      req.ip = requestIp.getClientIp(req)
+      return next()
+    },
+    // rate limiter
+    rateLimit({
+      windowMs: 1 * 60 * 1000, // 1 minutes
+      max: 60, // limit each IP to 60 requests per windowMs
+      handler: (req, res) => {
+        res.send('Too many requests, please try again later.', 429)
+      }
+    })
+  ],
+  routes: [{
+    prefix: '/public',
+    target: 'http://localhost:3000'
+  }]
+}).start(8080).then(() => console.log('API Gateway listening on 8080 port!'))
+
+const service = require('restana')({})
+service
+  .get('/hi', (req, res) => res.send('Hello World!'))
+  .start(3000).then(() => console.log('Public service listening on 3000 port!'))
