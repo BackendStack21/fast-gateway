1b040a2f736ae67bde6e62d427864a4dc435ce6f
Rolando Santamaria Maso <kyberneees@gmail.com>
2024-09-28 16:29:11 +0200

    extending Express.js demo

--- demos/basic-express.js
+++ demos/basic-express.js
@@ -1,3 +1,5 @@
+/* eslint-disable prefer-regex-literals */
+
 'use strict'
 
 const gateway = require('../index')
@@ -7,29 +9,35 @@ const PORT = process.env.PORT || 8080
 gateway({
   server: express(),
 
-  middlewares: [
-    require('cors')(),
-    require('helmet')()
-  ],
+  middlewares: [require('cors')(), require('helmet')()],
 
-  routes: [{
-    prefix: '/public',
-    target: 'http://localhost:3000',
-    docs: {
-      name: 'Public Service',
-      endpoint: 'swagger.json',
-      type: 'swagger'
+  routes: [
+    {
+      prefix: new RegExp('/public/.*'), // Express.js v5 requires a RegExp object
+      // prefix: '/public', // Compatible with Express.js v4,
+
+      urlRewrite: (req) => req.url.replace('/public', ''),
+      target: 'http://localhost:3000',
+      docs: {
+        name: 'Public Service',
+        endpoint: 'swagger.json',
+        type: 'swagger'
+      }
+    },
+    {
+      prefix: new RegExp('/admin/.*'), // Express.js v5 requires a RegExp object
+      // prefix: '/admin', // Compatible with Express.js v4,
+      target: 'http://localhost:3001',
+      middlewares: [
+        /*
+        require('express-jwt').expressjwt({
+          secret: 'shhhhhhared-secret',
+          algorithms: ['HS256'],
+        }),
+        */
+      ]
     }
-  }, {
-    prefix: '/admin',
-    target: 'http://localhost:3001',
-    middlewares: [
-      require('express-jwt')({
-        secret: 'shhhhhhared-secret',
-        algorithms: ['HS256']
-      })
-    ]
-  }]
+  ]
 }).listen(PORT, () => {
   console.log(`API Gateway listening on ${PORT} port!`)
 })
@@ -37,9 +45,11 @@ gateway({
 const service1 = require('restana')({})
 service1
   .get('/hi', (req, res) => res.send('Hello World!'))
-  .start(3000).then(() => console.log('Public service listening on 3000 port!'))
+  .start(3000)
+  .then(() => console.log('Public service listening on 3000 port!'))
 
 const service2 = require('restana')({})
 service2
-  .get('/users', (req, res) => res.send([]))
-  .start(3001).then(() => console.log('Admin service listening on 3001 port!'))
+  .get('/admin/users', (req, res) => res.send([]))
+  .start(3001)
+  .then(() => console.log('Admin service listening on 3001 port!'))
